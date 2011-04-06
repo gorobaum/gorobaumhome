e189d0643adfee929cc641a5ae139e10c4a39696
gorobaum <gorobaum@hotmail.com>
2011-04-06 16:24:47 -0300

    Pelo jeito Parse e Interp funfando. Falta fazer os contratos e erros.

--- conceitos/ep1/ep1-thiago-de-gouveia-nunes.rkt
+++ conceitos/ep1/ep1-thiago-de-gouveia-nunes.rkt
@@ -9,6 +9,8 @@
 
 (require racket/trace)
 
+;;  Definicoes  ;;
+
 (define-type Binding
   [binding (name symbol?) (named-expr WAE?)])
 
@@ -37,16 +39,53 @@
 ;; Definitions of the binop's used in the language.
 (define table (list (list '+ + ) (list '- -) (list '* *) (list '/ /)))
 
+;; Funcoes Auxiliares
 
 ;;  searchbinop: symbol table -> binop
 ;;  Consumes a symbol and a table and generates the binop which refers to the symbol.
 (define (searchbinop op table)
     (if (= (length table) 0 )
         (error 'searchbinop "Operacao binaria nao definida." )
-        (if (equal? (first (first table)))
+        (if (equal? (first (first table)) op)
             (second (first table))
             (searchbinop op (rest table)))))
-    
+
+
+;;  lookup : symbol DefrSub -> WAE
+;;  Consome um symbol e um DefrSub e devolve uma WAE
+(define (lookup name ds)
+    (type-case DefrdSub ds
+        [mtSub () (error 'lookup "no binding for identifier")]
+        [aSub (bound-name bound-value rest-ds)
+            (if (symbol=? bound-name name)
+                bound-value
+                (lookup name rest-ds))]))
+
+(define (binoperation op l r )
+    (if (equal? op /)
+        (if (= r 0)
+            (error 'binoperation "Tentativa de Divisao por zero!.")
+            ( op l r ))
+        ( op l r )))
+
+(define (mount-aSub lob ds)
+            (cond
+                [(empty? lob) ds]
+                [else (mount-aSub 
+                            (rest lob)
+                            (aSub (binding-name (first lob))
+                                (rinterp (binding-named-expr (first lob))
+                                ds)
+                            ds))]))
+
+(define (rinterp expr ds)
+    (type-case WAE expr
+        [num (n) n]
+        [binop (op l r) (binoperation op (rinterp l ds) (rinterp r ds ))]
+        [with (lob body) (rinterp body (mount-aSub lob ds))]
+        [id (v) (lookup v ds)]))
+
+;; Parse e Interp
 
 ;;  parse : s-exp -> WAE
 ;;  Consumes an s-expression and generates the corresponding WAE
@@ -57,26 +96,22 @@
         [(list? sexp)
             (if (= (length sexp) 3)
                 (case (first sexp)
-                    [(+) ( binop +  ( parse (second sexp) ) 
-                                    ( parse (third sexp) ) ) ]
-                    [(-) ( binop -  ( parse (second sexp) ) 
-                                    ( parse (third sexp) ) ) ]
-                    [(*) ( binop *  ( parse (second sexp) ) 
-                                    ( parse (third sexp) ) ) ]
-                    [(/) ( binop /  ( parse (second sexp) ) 
-                                    ( parse (third sexp) ) ) ]
                     [(with) ( with  ( lobmanager (second sexp) '() )
                                     ( parse (third sexp) ) ) ]
-                    [else ( error 'parse "Esperado um operador binario ou with." ) ] )
+                    [else ( binop   (searchbinop (first sexp) table)
+                                    (parse (second sexp))
+                                    (parse (third sexp)))])
             (error 'parse "Numero dos parametros para '+ '- ou with errado.") ) ]
         [else ( error 'parse "Entrada invalida para o parser." ) ] ) )
 
+;;
 
 (define (interp expr ) (rinterp expr (mtSub)))
 
+(trace mount-aSub)
 (trace rinterp)
 (trace searchbinop)
 (trace parse)
 (trace lobmanager)
 
-(parse(read))
+(interp(parse(read)))
